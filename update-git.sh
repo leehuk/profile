bbf8e4b105b2e2ecdad7a95d66e612f5362d5fa0
leeh <lee@leeh.uk>
2017-08-13 14:57:21 +0100

    - fixup install and directory cleanup

--- update-git.sh
+++ update-git.sh
@@ -4,20 +4,22 @@ git_clone_vimbundle() {
     dir=$1
     git=$2
 
-    rm -rf "vim/bundle/${dir}"
-    git clone --depth 1 ${git} "vim/bundle/${dir}"
+    git clone --depth 1 "${git}" "vim/bundle/${dir}"
     rm -rf "vim/bundle/${dir}/.git"
 }
 
-if [ ! -d 'vim/bundle' ]; then
-    echo "Error: $0 must run from base directory"
-    exit 1
-fi
+cd `dirname $0`
+
+rm -rf "vim/bundle"
+mkdir vim/bundle
 
 git_clone_vimbundle "nerdtree" "https://github.com/scrooloose/nerdtree.git"
 git_clone_vimbundle "vim-airline" "https://github.com/vim-airline/vim-airline.git"
 git_clone_vimbundle "vim-fugitive" "https://github.com/tpope/vim-fugitive.git"
 git_clone_vimbundle "vim-pathogen" "https://github.com/tpope/vim-pathogen.git"
 git_clone_vimbundle "vim-sensible" "https://github.com/tpope/vim-sensible.git"
+
+rm -rf "vim/autoload"
+mkdir "vim/autoload"
 
 cp "vim/bundle/vim-pathogen/autoload/pathogen.vim" "vim/autoload/pathogen.vim"
